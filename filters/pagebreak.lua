bd01765a88348352501a4c9115a561073778ad73
Penaz <penazarea@altervista.org>
2021-02-22 21:21:50 +0100

    Removed debug prints

--- filters/pagebreak.lua
+++ filters/pagebreak.lua
@@ -1,10 +1,8 @@
 local List = require 'pandoc.List'
 function pagebreak(elem)
     if elem.text == "{{pagebreak}}" then
-        print("Found")
         local x = {}
         if FORMAT:match 'latex' then
-            print("Run")
             table.insert(x, pandoc.RawInline('latex', '\\null\\clearpage'))
         end
         return x
