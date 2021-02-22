c090a7b44ed9c0330dd7d879315839817c354d08
Penaz <penazarea@altervista.org>
2021-02-22 21:46:19 +0100

    Added checkmark filter, but breaks tables

--- filters/filter_helper.lua
+++ filters/filter_helper.lua
@@ -1,9 +1,14 @@
 local boxes = require "filters/boxes"
 local placeholder = require "filters/placeholder"
 local pagebreak = require "filters/pagebreak"
+local symbols = require "filters/symbols"
 
 return {
-        {Div = boxes.boxes},
+        -- Inlines
         {Str = placeholder.placeholder},
-        {Str = pagebreak.pagebreak}
+        {Str = pagebreak.pagebreak},
+        {Str = symbols.symbols},
+        -- Blocks
+        {Div = boxes.boxes}
+        -- Meta
 }
---
+++ filters/symbols.lua
@@ -0,0 +1,14 @@
+local List = require 'pandoc.List'
+function symbols(elem)
+    if elem.text == "[[checkmark]]" then
+        local x = {}
+        if FORMAT:match 'latex' then
+            table.insert(x, pandoc.RawInline('latex', '\\checkmark'))
+        end
+        return x
+    else
+        return elem
+    end
+end
+
+return {symbols=symbols}
