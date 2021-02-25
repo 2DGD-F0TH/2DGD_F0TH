2bb98c06ebc0e4975fcd80ef5cd8213064af4605
Penaz <penazarea@altervista.org>
2021-02-25 22:09:38 +0100

    Metadata filters to ease transition to md front matter

--- filters/filter_helper.lua
+++ filters/filter_helper.lua
@@ -1,12 +1,15 @@
 local boxes = require "filters/boxes"
 local placeholder = require "filters/placeholder"
 local pagebreak = require "filters/pagebreak"
+local metavars = require "filters/metavars"
 
 return {
+        -- Meta
+        {Meta = metavars.get_vars},
         -- Inlines
+        {Str = metavars.replace},
         {Str = placeholder.placeholder},
         {Str = pagebreak.pagebreak},
         -- Blocks
         {Div = boxes.boxes}
-        -- Meta
 }
---
+++ filters/metavars.lua
@@ -0,0 +1,22 @@
+local vars = {}
+
+function get_vars (meta)
+  for k, v in pairs(meta) do
+    if type(v) == 'table' and v.t == 'MetaInlines' then
+      vars["{{%" .. k .. "%}}"] = {table.unpack(v)}
+    end
+    if type(v) == 'string' then
+      vars["{{%" .. k .. "%}}"] = tostring(v)
+    end
+  end
+end
+
+function replace (el)
+  if vars[el.text] then
+    return pandoc.Span(vars[el.text])
+  else
+    return el
+  end
+end
+
+return {get_vars=get_vars, replace=replace}
