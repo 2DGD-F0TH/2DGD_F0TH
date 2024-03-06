82ecbc721daf9fff59d356bd45758a3b9253508c
Penaz <penazarea@altervista.org>
2024-03-06 19:15:00 +0100

    Fixed c++ match3 code (used to be still pseudocode)

--- dynamic_listings/C++/default/developing_mechanics/match3_newtiles.cpp
+++ dynamic_listings/C++/default/developing_mechanics/match3_newtiles.cpp
@@ -1,16 +1,18 @@
-function createNewTiles() -> Map<Tile, int>{
+#include <unordered_map>
+
+std::unordered_map<Tile, int> createNewTiles(){
     // Our falling tiles list, will be used for tweening
-    Map<Tile, int> falling_tiles = {};
+    std::unordered_map<Tile, int> falling_tiles = {};
     // We scroll each column of the board
-    for (each column in matrix){
-        for (each tile in column){
-            if (tile == null){
-                Tile new_tile = new random Tile;
-                new_tile.y = - 64; // A value that is out of the board
+    for (Tile* column[] : matrix){
+        for (Tile* tile : column){
+            if (tile == nullptr){
+                Tile* new_tile = Tile::create_random();
+                new_tile->y = - 64; // A value that is out of the board
                 matrix[column][tile] = new_tile;
 
                 // Add this tile to the falling tiles mapping
-                falling_tiles[new_tile] = tile.y;
+                falling_tiles[new_tile] = tile->y;
             }
         }
     }
