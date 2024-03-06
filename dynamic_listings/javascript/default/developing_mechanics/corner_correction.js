ce65c9447aeadd8a92f339a152055e61b157448a
Penaz <penazarea@altervista.org>
2024-03-06 18:51:29 +0100

    Added corner correction javascript listings

---
+++ dynamic_listings/javascript/default/developing_mechanics/corner_correction.js
@@ -0,0 +1,29 @@
+function update(dt){
+    // Using the brute force checking for simplicity
+    for (const block in player.findCollisions(blocks)){
+        // ...
+        if (player.is_jumping){
+            // We are jumping, we need to check if we are ascending
+            // this way we will avoid "bonking our head" on a pixel
+            if (player.velocity.y < 0){
+                // We know we are ascending, let's check how far we are
+                // from the borders and react accordingly
+                if (player.position.x > block.rect.right - 5){
+                    // The player's left side is penetrating the block by
+                    // less than 5 pixels, let's correct it
+                    player.position.x = block.rect.right;
+                }else if (player.position.x + player.width < block.rect.left + 5){
+                    // The player's right side is penetrating the block by
+                    // less than 5 pixels, let's correct it
+                    player.position.x = block.rect.left - player.width;
+                }else{
+                    // The player is totally colliding with (bonking its head on) the block,
+                    // without need for corner correction, let's just act normally
+                    player.velocity.y = 0;
+                    player.position.y = block.rect.bottom;
+                }
+            }
+        }
+    }
+    // ...
+}
