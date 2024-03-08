a877f41ae48c0aff47a1b0b5808535bcb5845017
Penaz <penazarea@altervista.org>
2024-03-08 20:48:12 +0100

    Changed wording of {{extend}}

--- filters/placeholder.lua
+++ filters/placeholder.lua
@@ -3,7 +3,7 @@ function placeholder(elem)
     if elem.text == "{{placeholder}}" then
         return pandoc.Strong {pandoc.Emph {pandoc.Str "[This section is a work in progress and it will be completed as soon as possible]"}}
     elseif elem.text == "{{extend}}" then
-        return pandoc.Strong {pandoc.Emph {pandoc.Str "[Do you know more about this matter? You can contribute to this book!]"}}
+        return pandoc.Strong {pandoc.Emph {pandoc.Str "[Do you know more about this? You can contribute, this book is open source!]"}}
     else
         return elem
     end
