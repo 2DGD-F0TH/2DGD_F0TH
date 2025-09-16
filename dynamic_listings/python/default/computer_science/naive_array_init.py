5a9d4e3cae8c50a199e0bc0beb7fa73bd92ae437
Penaz <penazarea@altervista.org>
2025-09-16 20:31:20 +0200

    Added python listings for magic numbers and initialization

---
+++ dynamic_listings/python/default/computer_science/naive_array_init.py
@@ -0,0 +1,12 @@
+# ...
+tyler = []
+tyler.append(CharacterExpression.neutral)
+tyler.append(CharacterExpression.neutral)
+tyler.append(CharacterExpression.neutral)
+# 31 more lines
+tyler.append(CharacterExpression.angry)
+tyler.append(CharacterExpression.sad)
+tyler.append(CharacterExpression.neutral)
+# 26 more lines
+tyler.append(CharacterExpression.neutral)
+# ... Next character ...
---
+++ dynamic_listings/python/default/computer_science/smart_array_init.py
@@ -0,0 +1,7 @@
+# ...
+tyler_array_len = 64
+tyler = [CharacterExpression.neutral] * tyler_array_len
+# Here we take care of the exceptions
+tyler[33] = CharacterExpression.angry
+tyler[34] = CharacterExpression.sad
+# ... Next character ...
