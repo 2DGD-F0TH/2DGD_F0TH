ae1b7d4c4a85fdb51a0e57773f8e9e8df4c99a6e
Penaz <penazarea@altervista.org>
2025-09-17 21:05:16 +0200

    Added C++ code listings for magic numbers and default init

---
+++ dynamic_listings/C++/default/computer_science/naive_array_init.cpp
@@ -0,0 +1,12 @@
+// ...
+CharacterExpression* tyler = new CharacterExpression[64];
+tyler[0] = CharacterExpressions.neutral;
+tyler[1] = CharacterExpressions.neutral;
+tyler[2] = CharacterExpressions.neutral;
+// 31 more lines
+tyler[33] = CharacterExpressions.angry;
+tyler[34] = CharacterExpressions.sad;
+tyler[35] = CharacterExpressions.neutral;
+// 26 more lines
+tyler[63] = CharacterExpressions.neutral;
+// ... Next character ...
