76f48af7a6ca44d81cafe0cdb68adcb6a6e590f3
Penaz <penazarea@altervista.org>
2025-09-17 20:49:12 +0200

    Added JS code listings for magic numbers and default init

---
+++ dynamic_listings/javascript/default/computer_science/magic_numbers_2.js
@@ -0,0 +1,7 @@
+// ...
+const SCREEN_WIDTH = 640;
+const SCREEN_HEIGHT = 480;
+const BITS_PER_PIXEL = 32;
+const FULLSCREEN = false;
+screen.initialize(SCREEN_WIDTH, SCREEN_HEIGHT, BITS_PER_PIXEL, FULLSCREEN);
+// ...
